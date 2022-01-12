a4bfd336764311e398ef87d4b21cb789952cdfaf
BuGalter <wardoc78@gmail.com>
2022-01-12 12:10:04 +0300

    Added description to php max-in-array.php

--- php/max-in-array.php
+++ php/max-in-array.php
@@ -1,11 +1,16 @@
 <?php
+/**
+ * The function returns the largest number in the array.
+ * @param {Array<number>} arr - Maximum search array.
+ * @return {number} max - Maximum array element.
+ */
 function maxInArray($arr)
 {
   $max = null;
   foreach($arr as $value)
     {
-        if($value > $max or $max === null)
-            $max = $value;
+      if($value > $max or $max === null)
+        $max = $value;
     }
   return $max;
 }
