2b173d2a279bd82bb354e03388222d00fef77a2a
BuGalter <wardoc78@gmail.com>
2022-01-12 11:09:07 +0300

    Change in js prime-number.js < n+1 on <= n

--- js/prime-numbers.js
+++ js/prime-numbers.js
@@ -1,13 +1,13 @@
 function primeNumber() {
   const n = 100;
   let primeNumbers = [];
-  for (let i = 0; i < n + 1; i += 1) {
+  for (let i = 0; i <= n; i += 1) {
     primeNumbers[i] = i;
   };
-  for (let j = 2; j < n + 1; j += 1) {
+  for (let j = 2; j <= n; j += 1) {
     if (primeNumbers[j] !== 0) {
       console.log(primeNumbers[j]);
-      for (let k = j * j;  k < n + 1; k += j) {
+      for (let k = j * j;  k <= n; k += j) {
         primeNumbers[k] = 0;
       }
     }
