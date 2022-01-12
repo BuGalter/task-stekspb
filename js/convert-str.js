6d847fb6be08b38e598083a5e1ed14ea678eef01
BuGalter <wardoc78@gmail.com>
2022-01-12 11:55:27 +0300

    Added description to js max-in-array.js

--- js/convert-str.js
+++ js/convert-str.js
@@ -8,6 +8,7 @@ function convertString(str) {
    * The function takes a string of at most 100 characters and converts it. 
    * Replaces the space character in this string with "% 20" and
    * all Uppercase letters with lowercase letters.
+   * @param {string} str - String to konvert.
    * @returns The function does not return anything, the result is displayed on the screen.
    * If the string is more than 100 characters long, it returns an empty string.
    */
--- js/max-in-array.js
+++ js/max-in-array.js
@@ -1,4 +1,14 @@
+/**
+ * max-in-array module.
+ * @module task-stekspb/js/max-in-array
+ */
+
 function maxInArray(arr) {
+  /**
+   * The function returns the largest number in the array.
+   * @param {Array<number>} arr - Maximum search array.
+   * @returns {number} max - Maximum array element.
+   */
   let len = arr.length;
   let max = -Infinity;
   for (let i = 0; i < len; i += 1) {
