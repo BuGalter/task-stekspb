c28267c7f387a5d4d89253f00948f8b606abeaf4
BuGalter <wardoc78@gmail.com>
2022-01-12 12:12:36 +0300

    Added description to php convert-str.php

--- php/convert-str.php
+++ php/convert-str.php
@@ -1,8 +1,16 @@
 <?php
 function convert_str($str) {
+/**
+ * The function takes a string of at most 100 characters and converts it. 
+ * Replaces the space character in this string with "% 20" and
+ * all Uppercase letters with lowercase letters.
+ * @param {string} str - String to konvert.
+ * The function does not return anything, the result is displayed on the screen.
+ * If the string is more than 100 characters long, it returns an empty string.
+ */
     $resultstr = '';
     if (strlen($str) > 100) {
-      return $resultstr;
+        return $resultstr;
     };
     for ($i = 0; $i < strlen($str); $i++) {
         if ($str[$i] == ' ') {
